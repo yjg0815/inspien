bfd1ac3286fe6df9034425a1a7d7d63cdbaea61a
dlwlsrud815 <dlwlsrud815@gmail.com>
2026-07-05 19:08:17 +0900

    feat: shipment scheduler

---
+++ src/main/java/com/assignment/inspien/scheduler/ShipmentScheduler.java
@@ -0,0 +1,22 @@
+package com.assignment.inspien.scheduler;
+
+import com.assignment.inspien.service.ShipmentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ShipmentScheduler {
+
+    private final ShipmentService shipmentService;
+
+    @Scheduled(fixedDelayString = "${eai.batch.shipment-interval-ms}")
+    public void run() {
+        log.info("운송 배치 시작");
+        shipmentService.processShipment();
+        log.info("운송 배치 종료");
+    }
+}
